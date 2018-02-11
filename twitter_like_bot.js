29212d6a8fb21b1c5faef968ffb8e1b48d487b15
darshit021 <36310597+darshit021@users.noreply.github.com>
2018-02-12 01:38:18 +0530

    Update twitter_like_bot.js

--- twitter_like_bot.js
+++ twitter_like_bot.js
@@ -5,13 +5,13 @@ class MyEmitter extends EventEmitter {};
 const myEmitter = new MyEmitter();
 
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: 'eIfje6tRbFjrqqWwH3JISXQrB',
+  consumer_secret: 'qmm0OubgEhqMD7pTWUh3lQ8i78w5ZvVFmoNEP9ljZvy1M3LiDF',
+  access_token_key: '	959097430896553984-MJ4bDubbdU8AWmMPRNP5ekvFU7AnmjV',
+  access_token_secret: 'pOUCuL3uwYcYA3nLQXE0GGjAzE8AncAXAGuoCoDDBSRLy'
 });
 
-var keywords = {track:'keyword, keyword'};
+var keywords = {track:'hmmmmm'};
 var stream = client.stream('statuses/filter', keywords);
 
 myEmitter.on('event', (tweetId) => {
